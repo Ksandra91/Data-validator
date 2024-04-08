b19729b90aac45870c7c94f0355a1f713a8d524e
Aleksandra Volkova <91-ksandra@mail.ru>
2024-04-08 15:21:27 +0600

    Stage 2

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,4 +1,59 @@
 package hexlet.code.schemas;
 
 public class StringSchema {
+
+    private boolean isRequired = false;
+    private boolean isMin = false;
+    private int minLen;
+    private boolean isContains = false;
+    private String cont;
+
+
+    public StringSchema() {
+
+    }
+
+    public void required() {
+        this.isRequired = true;
+    }
+
+    public StringSchema minLength(int min) {
+        this.isMin = true;
+        this.minLen = min;
+        return this;
+    }
+
+    public StringSchema contains(String str) {
+        this.cont = str;
+        this.isContains = true;
+        return this;
+    }
+
+    public boolean isValid(String str) {
+        boolean param1 = true;
+        boolean param2 = true;
+        boolean param3 = true;
+
+        if (isRequired) {
+            if (str == null || str.isEmpty()) {
+                param1 = false;
+            }
+        }
+        if (isMin) {
+            assert str != null;
+            if (str.length() < minLen) {
+                param2 = false;
+            }
+        }
+
+        if (isContains) {
+            assert str != null;
+            if (!str.contains(cont)) {
+                param3 = false;
+            }
+        }
+
+        return param1 && param2 && param3;
+    }
+
 }
--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,4 +1,16 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ValidatorTest {
+    @Test
+
+    public void test1() {
+        var v = new Validator();
+        var schema = v.string();
+        boolean result = schema.minLength(10).minLength(4).isValid("Hexlet");
+        assertTrue(result);
+    }
 }
